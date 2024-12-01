6af9ccb2692340c49304de3437601e7d93f21fab
kul2105 <kul2105@gmail.com>
2024-12-01 15:49:31 +0530

    Add files via upload

---
+++ Cls/clsAccounts.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace M4.Cls
+{
+    class clsAccounts
+    {
+    }
+}
---
+++ Cls/clsQuotes.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace M4.Cls
+{
+    public class clsQuotes
+    {
+        public string Key { get; set; }
+        public string Symbol { get; set; }
+        public double BuyPrice { get; set; }
+        public double SellPrice { get; set; }
+        //public int Digits { get; set; }
+        public Int32 ProfitLoss { get; set; }
+        public double PCT { get; set; }
+        public double HighValue { get; set; }
+        public double LowValue { get; set; }
+        public double AveragePrice { get; set; }
+        public Int32 Position { get; set; }
+    }
+}
